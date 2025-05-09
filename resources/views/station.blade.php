d05231acadb676ebd6c7bd6a91d792e3e2460759
whei2419 <87625823+whei2419@users.noreply.github.com>
2025-05-10 00:44:44 +0800

    update error image source in sendMessage function for improved UX

--- resources/views/station.blade.php
+++ resources/views/station.blade.php
@@ -197,7 +197,7 @@
                     console.error('Error sending QR Code message:', error);
                     $('.station-text').html('Failed');
                     $('.message').html('Invalid QR code. Please try again.');
-                    $('.check').attr('src', '{{ asset('error.webp') }}');
+                    $('.check').attr('src', '{{ asset('images/error.webp') }}');
                     $(scanCompleteModal).modal('show');
                 }
             });

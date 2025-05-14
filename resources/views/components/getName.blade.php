9e755d00f7d294c5e70d41b6ac009ecaf9dd4b0a
whei2419 <87625823+whei2419@users.noreply.github.com>
2025-05-14 17:52:57 +0800

    fix: update name help text styling for improved visibility

--- resources/views/components/getName.blade.php
+++ resources/views/components/getName.blade.php
@@ -2,7 +2,7 @@
     <h1 class="text-center">PLEASE INSERT YOUR NAME</h1>
     <div class="input-container px-5">
         <input type="text" class="form-control rounded-pill text-center" id="name" maxlength="5" aria-describedby="nameHelp" placeholder="your name">
-        <div id="nameHelp" class="form-text text-center">*Maximum 5 character</div>
+        <div id="nameHelp" class="form-text text-center mt-2">*Maximum 5 character</div>
     </div>
     <div class="next-button-container text-center">
         <button onclick="addName()" class="next-button"><span>Next</span></button>

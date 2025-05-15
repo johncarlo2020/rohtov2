f32908dbee2320f4a3d4cbbc5e95cd72cd2f5f18
johncarlo2020 <johncarlocasipit@gmail.com>
2025-05-15 10:34:00 +0000

    change

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -7,7 +7,7 @@
         <meta name="csrf-token" content="{{ csrf_token() }}" />
         <meta name="csrf-token" content="{{ csrf_token() }}" />
 
-        <title>{{ config("app.name", "Hadalabo Experience") }}</title>
+        <title>{{ config("app.name", "Ocean or Plastic") }}</title>
 
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net" />

03665d1d9949f7bdb4146d0e76253c715ad951e6
whei2419 <87625823+whei2419@users.noreply.github.com>
2025-05-16 13:45:23 +0800

    Update appointment view to improve venue display formatting

--- resources/views/appointment.blade.php
+++ resources/views/appointment.blade.php
@@ -82,7 +82,7 @@
 
             </div>
 
-            <p class="sub-heading-text text-center mb-0">Date selected: <span id="selected-date">{{ $selectedAppointment->appointment->name ?? '' }}</span>, Wednesday Venue: IOI City Mall,
+            <p class="sub-heading-text text-center mb-0">Date selected: <span id="selected-date">{{ $selectedAppointment->appointment->name ?? '' }}</span>, Wednesday <br/> Venue: IOI City Mall,
                 Putrajaya – West Court on Ground Floor</p>
             <div class="p-3">
                 <p class="pharagraph-text mb-0"><Strong>Terms & Conditions</Strong></p>

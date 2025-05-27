b813bc95f12c5c438800e4fbf5364809669e3b3f
whei2419 <87625823+whei2419@users.noreply.github.com>
2025-05-27 11:01:09 +0800

    Refactor dashboard layout and improve responsiveness
    - Updated the dashboard view to enhance the layout and responsiveness for mobile devices.
    - Added a new "Scanner" card for quick access on smaller screens.
    - Adjusted the structure of customer statistics cards for better alignment and spacing.
    - Modified the chart containers to ensure proper rendering and responsiveness.
    - Updated the sidebar navigation to improve usability and accessibility.

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -48,7 +48,7 @@
 <body class="g-sidenav-show bg-gray-100">
     <div class="min-height-300 bg-primary position-absolute w-100"></div>
     <aside
-        class="sidenav bg-white navbar navbar-vertical navbar-expand-xs border-0 border-radius-xl my-3 fixed-start ms-4"
+        class="sidenav bg-white navbar navbar-vertical navbar-expand-xs collapse d-xl-block border-0 border-radius-xl my-3 fixed-start ms-4"
         id="sidenav-main">
         <div class="sidenav-header">
             <i class="fas fa-times p-3 cursor-pointer text-secondary opacity-5 position-absolute end-0 top-0 d-none d-xl-none"
@@ -61,7 +61,7 @@
         <div class="collapse navbar-collapse w-auto" id="sidenav-collapse-main">
             <ul class="navbar-nav">
                 <li class="nav-item">
-                    <a class="nav-link {{ request()->routeIs('admin') ? 'active' : '' }}" href="{{ route('admin') }}">
+                    <a class="nav-link  w-100 {{ request()->routeIs('admin') ? 'active' : '' }}" href="{{ route('admin') }}">
                         <div
                             class="icon icon-shape icon-sm border-radius-md text-center me-2 d-flex align-items-center justify-content-center">
                             <i class="ni ni-tv-2 text-primary text-sm opacity-10"></i>

9e841d09b261a14cbe5eb950183312aeecf08cd3
whei2419 <87625823+whei2419@users.noreply.github.com>
2025-07-01 02:17:45 +0800

    feat: update branding and button styles; refactor welcome page layout and improve responsiveness

--- resources/views/components/branding.blade.php
+++ resources/views/components/branding.blade.php
@@ -1,6 +1,6 @@
 <a href="{{ route('dashboard') }}">
     <div class="branding">
         <!-- <img class="logo" src="{{ asset('images/hadalabobabies/logo.webp') }}" alt="" /> -->
-        <img class="welcome_img w-50 img-fluid" src="{{ asset('images/dutchlady/dutchLadyLogo.png') }}" alt="" />
+        <img class="logo" src="{{ asset('images/dutchlady/dutchLadyLogo.png') }}" alt="" />
     </div>
 </a>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -22,7 +22,7 @@
             font-weight: 900;
             font-style: normal;
             }
-    
+
     body,
     html {
         height: 100%;
@@ -68,15 +68,14 @@
     .sign-up-btn-txt {
   font-family: 'HvDTrial Brevia', sans-serif;
   font-weight: 900; /* optional, reinforces the ExtraBlack weight */
-  
+
 }
 </style>
 
 <body class="antialiased welcome-page main-background hadalabo">
-    <div class="py-5 container-fluid main-content">
+    <div class="py-3 container-fluid main-content">
         <div class="row">
             <div class="col-12 d-flex justify-content-center align-items-center">
-                <!-- <img class="welcome_img w-50 img-fluid" src="{{ asset('images/dutchlady/dutchLadyLogo.png') }}" alt="" /> -->
                 @include('components.branding')
             </div>
             <div class="col-12 d-flex justify-content-center align-items-center">
@@ -85,20 +84,15 @@
             <div class="col-12 d-flex justify-content-center align-items-center p-0">
                 <img class="welcome_img_store" src="{{ asset('images/dutchlady/dutchLadyLoginImg.webp') }}" alt="" />
             </div>
-            <div class="text-center bottom-text-welcome col-12 mt-5">
+            <div class="text-center bottom-text-welcome col-12 mt-4">
                 <div class="d-block">
                     <div class="col mb-3">
-                        <a href="{{ route('register') }}" class="home-btn welcome-sign-btn btn rounded-pill border-white"><span class="sign-up-btn-txt">Sign Up</span></a>
+                        <a href="{{ route('register') }}" class="button-dutch button-dutch button-dutch-primary"><span class="sign-up-btn-txt">Sign Up</span></a>
                     </div>
                     <div class="col">
-                        <a href="{{ route('login') }}" class="home-btn welcome-sign-btn btn rounded-pill border-primary bg-white"><span class="text-primary">Login</span></a>
+                        <a href="{{ route('login') }}" class="button-dutch button-dutch button-dutch-outlined"><span class="text-primary">Login</span></a>
                     </div>
                 </div>
-                <!-- <p class="mt-5 p-0 m-0">Already Registered</p>
-                <p class="m-0 p-0">
-                    Please Login
-                    <a class="underline" href="{{ route('login') }}" class="">here</a>
-                </p> -->
             </div>
         </div>
     </div>

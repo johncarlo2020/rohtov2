74d3e53fabc9e5831bbbf5f8911d76e8b284dd88
johncarlo2020 <johncarlocasipit@gmail.com>
2025-07-04 06:02:39 +0000

    Update logo path in admin layout to new directory structure

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -53,7 +53,7 @@
             <i class="fas fa-times cursor-pointer text-secondary opacity-5 position-absolute end-0 top-0 d-none d-xl-none"
                 aria-hidden="true" id="iconSidenav"></i>
             <a class="navbar-brand m-0 d-flex w-100 justify-content-center" href="" target="_blank">
-                <img src="{{ asset('images/logo.webp') }}" class="navbar-brand-img h-100" alt="main_logo" />
+                <img src="{{ asset('images/hadalabobabies/logo.webp') }}" class="navbar-brand-img h-100" alt="main_logo" />
             </a>
         </div>
         <hr class="horizontal dark mt-0" />

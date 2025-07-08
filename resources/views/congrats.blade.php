5b49c7f8195df9bee3b2cb5fa2601711037e512e
rgabuat <johncarlgabuat@gmail.com>
2025-07-08 17:36:36 +0800

    congrats page

--- resources/views/congrats.blade.php
+++ resources/views/congrats.blade.php
@@ -1,22 +1,24 @@
 <x-app-layout>
     <div class="container-fluid home start completed-screen pt-4">
-
         <div class="congrats-container">
             <div class="col-12 d-flex justify-content-center align-items-center mt-3">
                 <img class="welcome_img w-75" src="{{ asset('images/dutchlady/thankYouMessage.webp') }}" alt="" />
             </div>
-            <div class="product-image mb-3 px-5">
-                <a href="">
+                <div class="product-image mb-3 px-5 text-center">
+                    <span >Visit</span>
+                    <a href="">
 
-                    <img class="logo "
-                        src="{{ asset('images/dutchlady/dutchLadyLogo.png') }}" alt="Dutch Lady Logo" />
+                        <img class="logo "
+                            src="{{ asset('images/brand/logo.webp') }}" alt="sekkisei Logo" />
 
-                </a>
-            </div>
-            <div class="next-button-container text-center mt-5">
-                <button onclick="window.location.href='{{ route('dashboard') }}'" class="button-dutch button-dutch-primary"><span>Done</span></button>
+                    </a>
+                    <span>for more information</span>
+                </div>
+                <div class="next-button-container text-center mt-5">
+                    <button onclick="window.location.href='{{ route('dashboard') }}'" class="custom-btn custom-btn-secondary"><strong class="text-dark">BACK</strong></button>
+                </div>
             </div>
         </div>
-    </div>
+        <x-footer />
     </div>
 </x-app-layout>

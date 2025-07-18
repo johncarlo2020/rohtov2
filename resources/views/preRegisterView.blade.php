79be91109dd37521d33b11e4b935b03d0e41a029
johncarlo2020 <johncarlocasipit@gmail.com>
2025-07-18 07:18:02 +0000

    fix: Update availability message date in pre-register view

--- resources/views/preRegisterView.blade.php
+++ resources/views/preRegisterView.blade.php
@@ -50,7 +50,7 @@
                         <div class="map-img mb-3">
                             <img src="{{ asset('files/main/locci map_np shadow.webp') }}" alt="" />
                         </div>
-                        <p class="modal-main-text mb-4 px-2">This section will be available starting 27 July, see you
+                        <p class="modal-main-text mb-4 px-2">This section will be available starting 28 July, see you
                             then!</p>
                         {{-- <p class="warning-text text-center px-5">Note: You may reschedule your selected date
                         <strong>only once</strong>.

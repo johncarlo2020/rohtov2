395a233e8a38006f593e2c1caad6b02a0ec13499
johncarlo2020 <77434763+johncarlo2020@users.noreply.github.com>
2025-10-28 12:33:41 +0800

    Add text color to visit links for better visibility in congrats view

--- resources/views/congrats.blade.php
+++ resources/views/congrats.blade.php
@@ -5,20 +5,20 @@
                 <img class="welcome_img w-75" src="{{ asset('images/dutchlady/thankYouMessage.webp') }}" alt="" />
             </div>
                 <div class="product-image mb-3 px-5 text-center">
-                    <span >Visit</span>
+                    <span class="text-dark">Visit</span>
                     <a href="">
 
                         <img class="logo "
                             src="{{ asset('images/brand/logo.webp') }}" alt="sekkisei Logo" />
 
                     </a>
-                    <span>for more information</span>
+                    <span class="text-dark">for more information</span>
                 </div>
                 <div class="next-button-container text-center mt-5">
                     <button onclick="window.location.href='{{ route('dashboard') }}'" class="custom-btn custom-btn-secondary"><strong class="text-dark">BACK</strong></button>
                 </div>
             </div>
         </di>
-        <x-footer />
+
     </div>
 </x-app-layout>

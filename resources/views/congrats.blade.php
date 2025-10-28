562cda8ba1fdbad1fb0d9c4aad5f7090764c8916
johncarlo2020 <77434763+johncarlo2020@users.noreply.github.com>
2025-10-28 17:06:52 +0800

    Fix HTML structure in congrats view by correcting div tag and improving layout consistency

--- resources/views/congrats.blade.php
+++ resources/views/congrats.blade.php
@@ -1,24 +1,26 @@
 <x-app-layout>
-    <di v class="container-fluid congrats start completed-screen main-content main-background with-scroll pt-4">
+    <div class="container-fluid congrats start completed-screen main-content main-background with-scroll pt-4">
         <div class="congrats-container">
             <div class="col-12 d-flex justify-content-center align-items-center mt-3">
                 <!-- <img class="welcome_img w-75" src="{{ asset('images/dutchlady/thankYouMessage.webp') }}" alt="" /> -->
             </div>
-                <div class="product-image mb-3 px-5 text-center">
-                    <span class="text-dark">Visit</span>
-                    <a href="">
-
+                <div class="product-image px-5 text-center">
+                    <div class="mb-4">
+                        <span class="text-dark">Visit</span>
+                    </div>
+                    <a href="https://www.rytbank.my/" class="mt-5">
                         <img class="logo "
                             src="{{ asset('images/brand/logo.webp') }}" alt="sekkisei Logo" />
-
                     </a>
-                    <span class="text-dark">for more information</span>
+                    <div class="mt-4">
+                        <span class="text-dark">for more information</span>
+                    </div>
                 </div>
                 <div class="next-button-container text-center mt-5">
                     <button onclick="window.location.href='{{ route('dashboard') }}'" class="custom-btn custom-btn-secondary"><strong class="text-dark">BACK</strong></button>
                 </div>
             </div>
-        </di>
+        </div>
 
     </div>
 </x-app-layout>

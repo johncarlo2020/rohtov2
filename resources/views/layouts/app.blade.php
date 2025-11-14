7d4a24b799e23944b4f00a699970d214d45f9ca5
whei2419 <87625823+whei2419@users.noreply.github.com>
2025-11-15 00:58:15 +0800

    test fix

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -3,8 +3,12 @@
 
 <head>
     <meta charset="utf-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1, viewport-fit=cover">
+    <meta name="viewport" content="width=device-width, initial-scale=1, viewport-fit=cover, user-scalable=no">
     
+    <!-- iOS specific meta tags -->
+    <meta name="apple-mobile-web-app-capable" content="yes">
+    <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent">
+    <meta name="format-detection" content="telephone=no">
 
     <!-- CSRF Token for security -->
     <meta name="csrf-token" content="{{ csrf_token() }}" />

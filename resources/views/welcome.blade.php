3ba3e387b35af00a61244edeabfbe4875f47579a
rgabuat <73892616+rgabuat@users.noreply.github.com>
2026-01-16 12:18:00 +0800

    landing page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -69,31 +69,12 @@
 
 <body class="antialiased welcome-page" style="background-image:url('{{ asset('images/brand/landing_bg.webp') }}');">
 
-    <div class="container-fluid main-content with-scroll px-0 py-4">
+    <div class="container-fluid main-content with-scroll px-5 py-4">
         <div class="top-container">
             <!-- Branding (top area) -->
             <div class="row flex-grow-1">
                 <div class="col-12 animate-entry mb-4">
                     @include('components.branding')
-                </div>
-            </div>
-        </div>
-        <div class="text-container">
-            <div class="row">
-                <div class="animate-entry">
-                    <img class="welcome_img_store w-100" 
-                        src="{{ asset('images/brand/text_front.webp') }}" 
-                        alt="">
-                </div>
-            </div>
-        </div>
-        <div class="main-image-container">
-            <!-- Center image (middle area) -->
-            <div class="row">
-                <div class="animate-entry mb-5">
-                    <img class="welcome_img_store w-100" 
-                        src="{{ asset('images/brand/image_front.webp') }}" 
-                        alt="">
                 </div>
             </div>
         </div>
@@ -104,23 +85,11 @@
                     <div class="d-block  mb-2">
                         <div class="colanimate-entry delay-2 btn-wrapperx">
                             <a href="{{ route('register') }}" class="custom-btn custom-btn-primary pulse-slow">
-                                DAFTAR
-                            </a>
-                        </div>
-                    </div>
-                    <div class="d-block">
-                        <div class="colanimate-entry delay-2 btn-wrapperx">
-                            <a href="{{ route('login') }}" class="custom-btn custom-btn-secondary pulse-slow">
-                                LOG MASUK
+                                JOIN NOW
                             </a>
                         </div>
                     </div>
                 </div>
-            </div>
-
-            <!-- Footer -->
-            <div class="footer-welcome mb-4 text-center w-100">
-                <x-footer/>
             </div>
         </div>
     </div>

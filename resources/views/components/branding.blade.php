1ee325358dc1ff29e4f9f502713b56db293c4161
rgabuat <johncarlgabuat@gmail.com>
2026-09-15 16:38:42 +0800

    remove logo

--- resources/views/components/branding.blade.php
+++ resources/views/components/branding.blade.php
@@ -1,5 +1,5 @@
-<div class="branding-header desktop-only-logo">
-    <div class="branding text-center">
+<div class="branding-header desktop-only-logo d-none">
+    <div class="branding text-center mb-5">
         <img class="logo" src="{{ asset('images/brand/logo1.webp') }}" alt="Longchamp" />
     </div>
 </div>

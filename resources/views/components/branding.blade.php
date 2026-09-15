6e55cf706bfedfcf3a0456b4434b5764c9b8282f
rgabuat <johncarlgabuat@gmail.com>
2026-09-15 16:15:47 +0800

    change logo

--- resources/views/components/branding.blade.php
+++ resources/views/components/branding.blade.php
@@ -1,5 +1,5 @@
 <div class="branding-header desktop-only-logo">
     <div class="branding text-center">
-        <img class="logo" src="{{ asset('images/brand/logo.webp') }}" alt="Longchamp" />
+        <img class="logo" src="{{ asset('images/brand/logo1.webp') }}" alt="Longchamp" />
     </div>
 </div>
